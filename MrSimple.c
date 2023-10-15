b60730d0cde94853b83d31919544a08be27423bd
Dave Cadilo <davecadilo@gmail.com>
2023-10-15 11:07:58 -0700

    Initial commit

---
+++ MrSimple.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,ans=0;
+    float ans1 =0;
+    char choice;
+
+    printf("\nA for Addition\n ");
+    printf("S for Subtraction\n ");
+    printf("M for Multiplication\n ");
+    printf("D for Division\n ");
+
+    printf("\nEnter Operation of Choice: ");
+    scanf("%c",&choice);
+
+    return 0;
+}
